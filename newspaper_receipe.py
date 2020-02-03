89e56515360b74668c2a75fb01a58abf01149985
Junior Aguayo <junior.aguayo.cuadros@gmail.com>
2020-02-02 22:21:51 -0500

    Added newspaper receipe file

---
+++ newspaper_receipe.py
@@ -0,0 +1,16 @@
+import argparse
+import logging
+logging.basicConfig(level=logging.INFO)
+from urllib.parse import urlparse
+
+import pandas as pd
+
+
+logger = logging.getLogger(__name__)
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
