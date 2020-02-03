9a97cf835ca314e7a18f461e4539d83eed70bd6a
Junior Aguayo <junior.aguayo.cuadros@gmail.com>
2020-02-02 22:38:45 -0500

    Added read data

--- newspaper_receipe.py
+++ newspaper_receipe.py
@@ -8,9 +8,26 @@ import pandas as pd
 
 logger = logging.getLogger(__name__)
 
-def main():
-    pass
+
+def main(filename):
+    logger.info('Starting cleaning process')
+
+    df = _read_data(filename)
+
+    return df
+
+
+def _read_data(filename):
+    logger.info('reading file {}'.format(filename))
+
+    return pd.read_csv(filename)
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename',
+                        help='The path to the dirty data',
+                        type=str)
+    args = parser.parse_args()
+    df = main(args.filename)
+    print(df)
